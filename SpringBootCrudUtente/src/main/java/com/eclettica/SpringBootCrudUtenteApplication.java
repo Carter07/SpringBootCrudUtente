1cf0e5720186c8f505cb2c025f666652a7c36a78
Carter07 <daredavid95@gmail.com>
2020-09-23 11:58:29 +0200

    Commit stashed file.
    Signed-off-by: Carter07 <daredavid95@gmail.com>

--- SpringBootCrudUtente/src/main/java/com/eclettica/SpringBootCrudUtenteApplication.java
+++ SpringBootCrudUtente/src/main/java/com/eclettica/SpringBootCrudUtenteApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootCrudUtenteApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCrudUtenteApplication.class, args);
+		SpringApplication.run(SpringBootCrudUtenteApplication.class, args); //
 	}
 
 }
